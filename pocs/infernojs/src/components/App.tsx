05067b62e625ee19fc45d6441da67e9448f98fde
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-03-27 23:13:53 -0300

    created routes with inferno-router

--- pocs/infernojs/src/components/App.tsx
+++ pocs/infernojs/src/components/App.tsx
@@ -1,24 +1,69 @@
-import { version, Component } from 'inferno';
-import Logo from './Logo';
+import { BrowserRouter, Route, Switch, Link, useLoaderData, useLoaderError } from 'inferno-router';
 
-class App extends Component {
-    render() {
-        return (
-            <header class="bg-slate-800 h-full text-white flex justify-center items-center flex-col">
-                <Logo width="160" height="160" />
-                <p class="mt-6">{`Welcome to Inferno ${version}`}</p>
-                <ul>
-                    <li>Compiled using SWC</li>
-                    <li>Styled using Tailwind</li>
-                    <li>Bundled using Webpack</li>
-                    <li>Linted using ESLint</li>
-                </ul>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-            </header>
-        );
-    }
-}
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
 
-export default App;
+const About = (props) => {
+    const data = useLoaderData(props);
+    const err = useLoaderError(props);
+
+    return (
+        <div>
+            <h2>About</h2>
+            <p>{data?.body || err?.message}</p>
+        </div>
+    )
+};
+
+const Topic = ({ match }) => (
+    <div>
+        <h3>{match.params.topicId}</h3>
+    </div>
+);
+
+const Topics = ({ match }) => (
+    <div>
+        <h2>Topics</h2>
+        <ul>
+            <li>
+                <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/components`}>Components</Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+            </li>
+        </ul>
+
+        <Route path={`${match.url}/:topicId`} component={Topic} />
+        <Route
+            exact
+            path={match.url}
+            render={() => <h3>Please select a topic.</h3>}
+        />
+    </div>
+);
+
+const MyWebsite = () => (
+    <BrowserRouter>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/topics">Topics</Link></li>
+            </ul>
+            <hr />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} loader={() => fetch(new URL('/api/about', 'http://localhost:5110'))} />
+                <Route path="/topics" component={Topics} />
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default MyWebsite;
