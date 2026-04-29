690fe75b847e717c5a3d4093678d9c16d87c154e
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-04-29 15:57:55 -0300

    add Class component

---
+++ pocs/es6-tests/src/components/Class.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+class MyComponentWithClass extends React.Component {
+    render() { return <div>MyComponentWithClass</div> }
+}
+
+export default MyComponentWithClass;
