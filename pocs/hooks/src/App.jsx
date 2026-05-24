8da6f90046eafe7bf2ebde15bbcf9f1cf97be592
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-05-24 20:23:15 -0300

    called the hook order

---
+++ pocs/hooks/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import UserCard from './components/UserCard'
+import HookOrder from './components/HookOrder'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <UserCard />
+        <br />
+        <HookOrder/>
+      </section>
+    </>
+  )
+}
+
+export default App
