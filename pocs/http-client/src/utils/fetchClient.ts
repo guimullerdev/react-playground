c70cd52d1d7ee1c57a727bc3b3416b79c452809b
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-05-27 17:28:37 -0300

    update fetchClient

--- pocs/http-client/src/utils/fetchClient.ts
+++ pocs/http-client/src/utils/fetchClient.ts
@@ -7,13 +7,41 @@ const config = {
 };
 
 async function client<T>(path: string, options?: RequestInit): Promise<T> {
-  const res = await fetch(`${config.baseUrl}${path}`, {
+  const modifiedOptions = requestInterceptor(options);
+  let response: Response;
+  
+  try {
+    response = await fetch(`${config.baseUrl}${path}`, {
+      ...modifiedOptions,
+      headers: {
+        ...config.headers,
+        ...modifiedOptions?.headers,
+      },
+    });
+  } catch (err) {
+    throw new Error(`Network error: ${(err as Error).message}`);
+  }
+
+  return responseInterceptor<T>(response);
+}
+
+function requestInterceptor(options?: RequestInit): RequestInit {
+  const token = localStorage.getItem("token");
+  return {
     ...options,
     headers: {
-      ...config.headers,
       ...options?.headers,
+      ...(token && { Authorization: `Bearer ${token}` }),
     },
-  });
+  };
+}
+
+async function responseInterceptor<T>(res: Response): Promise<T> {
+  if (res.status === 401) {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+    throw new Error("Unauthorized");
+  }
 
   if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
 
