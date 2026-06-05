1be5bf7fe63b18f25754dc75c7e113e607760644
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-06-04 21:45:07 -0300

    add auth routes with protected route

--- pocs/routing/src/react-router/Router.jsx
+++ pocs/routing/src/react-router/Router.jsx
@@ -1,14 +1,39 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AuthProvider } from '../auth/AuthContext'
+import { ProtectedRoute, protectedLoader } from '../auth/ProtectedRoute'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Profile from './pages/Profile'
 import TodoDetail from './pages/TodoDetail'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/todos/:id',
+    element: <TodoDetail />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    element: <ProtectedRoute />,
+    loader: protectedLoader,
+    children: [
+      { path: '/dashboard', element: <Dashboard /> },
+      { path: '/profile', element: <Profile /> },
+    ],
+  },
+])
 
 export default function ReactRouterApp() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/todos/:id" element={<TodoDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   )
 }
